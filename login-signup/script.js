f872da41ff375466ccd18a2a89201ee46b815bc9
Soham <92096348+Soham24122003@users.noreply.github.com>
2023-03-06 20:03:02 +0530

    Add files via upload
    The login and signup page that will pop up

---
+++ login-signup/script.js
@@ -0,0 +1,19 @@
+const signupButton = document.getElementById('signup-button'),
+    loginButton = document.getElementById('login-button'),
+    userForms = document.getElementById('user_options-forms')
+
+/**
+ * "Sign Up" button
+ */
+signupButton.addEventListener('click', () => {
+    userForms.classList.remove('bounceRight')
+    userForms.classList.add('bounceLeft')
+}, false)
+
+/**
+ * "Login" button
+ */
+loginButton.addEventListener('click', () => {
+    userForms.classList.remove('bounceLeft')
+    userForms.classList.add('bounceRight')
+}, false)
